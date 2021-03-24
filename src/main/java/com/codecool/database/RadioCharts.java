b639c6fd8280c1adcbb13e2138f6be1279526292
deszus <68805470+deszus@users.noreply.github.com>
2021-03-24 17:47:45 +0100

    2nd upload
    still 6/8 test and can't see why :S

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -14,13 +14,11 @@ public class RadioCharts {
         this.DB_PASSWORD = DB_PASSWORD;
     }
 
-    // adja vissza annak a számnak a címét a nevét, amelyiket az évben a legtöbbször játszottak a rádiók.
     public String getMostPlayedSong() {
         return getRequiredData("SELECT song, SUM (times_aired) FROM music_broadcast GROUP BY song ORDER BY SUM (times_aired) DESC LIMIT 1;",
                 "song");
     }
 
-    // adja vissza annak az előadónak a nevét, akinek a legtöbb száma szerepel az adatbázisban (ugyanaz a szám többször lejátszva egynek számít).
     public String getMostActiveArtist() {
         return getRequiredData("SELECT artist, COUNT (artist) FROM music_broadcast GROUP BY artist ORDER BY COUNT (artist) DESC LIMIT 1;",
                 "artist");
@@ -31,7 +29,7 @@ public class RadioCharts {
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             PreparedStatement st = conn.prepareStatement(sql);
             ResultSet rs = st.executeQuery();
-            while (rs.next()) {
+            if (rs.next()) {
                 result = rs.getString(fieldName);
             }
             rs.close();
